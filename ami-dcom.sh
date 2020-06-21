cb173b37ed7cc8237261acec548474bfe492c525
NoCtrlZ <phantomofrotten@gmail.com>
2020-06-21 19:49:30 +0900

    remove .bashrc file

--- ami-dcom.sh
+++ ami-dcom.sh
@@ -7,4 +7,5 @@ sudo usermod -a -G docker ec2-user &&\
 sudo curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&\
 sudo chmod +x /usr/local/bin/docker-compose
 sudo yum install git -y
+rm .bashrc
 wget https://raw.githubusercontent.com/NoCtrlZ/dotfiles/master/.bashrc .bashrc
